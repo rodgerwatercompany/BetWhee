b0a12133101a9cf18c26942c689e226145c420aa
rodgerwatercompany1 <rebornyschen@gmail.com>
2015-06-12 18:06:42 +0800

    2015/6/12

---
+++ Assets/manager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Rodger;
+
+public class manager : MonoBehaviour {
+
+    public BetWheel betWheel;
+
+	// Use this for initialization
+	void Start () {
+
+        betWheel.onChageBetNumCB = OnBetNumChange;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("Hide"))
+        {
+            betWheel.HideBetButton();
+        }
+        if (GUILayout.Button("Show"))
+        {
+            betWheel.ShowBetButton();
+        }
+    }
+    void OnBetNumChange(int value)
+    {
+        print("New bet num is " + value);
+    }
+}
